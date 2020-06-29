d0d773366c6c5c6ec80c65a6856e5dbc7c93283d
Florian Sloot <flosloot@gmx.de>
2020-06-29 17:59:16 +0200

    [SCRIPT] add second iteration of ci script

--- ci_script.sh
+++ ci_script.sh
@@ -4,3 +4,8 @@
 # the idea here is to get a incremental version number
 # the zip's name can be anything you like
 zip -r app_v_$CIRCLE_BUILD_NUM.zip .ebextensions/ Dockerrun.aws.json
+
+# installing the AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
